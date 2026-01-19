49a2d7a357e255753d5ffe25f20dd89d5daf18a2
Max Poletaev <max.poletaev@gmail.com>
2026-01-19 04:30:40 +0300

    SPU interpolation

--- src/adpcm.zig
+++ src/adpcm.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const adpcm_filter_table_pos = [5]i32{ 0, 60, 115, 98, 122 };
 const adpcm_filter_table_neg = [5]i32{ 0, 0, -52, -55, -60 };
 
-pub fn decodeBlock(block: *const [16]u8, prev_sample: *[2]i32, out: *[28]i16) void {
+pub fn decodeBlock(block: *const [16]u8, prev_samples: *[2]i32, out: *[28]i16) void {
     var shift = block[0] & 0x0f;
     var filter = (block[0] & 0x70) >> 4;
 
@@ -22,11 +22,11 @@ pub fn decodeBlock(block: *const [16]u8, prev_sample: *[2]i32, out: *[28]i16) vo
         var sample: i32 = @as(i32, nibble) << 12;
         sample >>= @intCast(shift);
 
-        sample += @divTrunc(prev_sample[0] * filter_pos + prev_sample[1] * filter_neg + 32, 64);
+        sample += @divTrunc(prev_samples[0] * filter_pos + prev_samples[1] * filter_neg + 32, 64);
 
         out[n] = @intCast(std.math.clamp(sample, -0x8000, 0x7fff));
 
-        prev_sample[1] = prev_sample[0];
-        prev_sample[0] = sample;
+        prev_samples[1] = prev_samples[0];
+        prev_samples[0] = sample;
     }
 }
