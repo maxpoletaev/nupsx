9b271c96adc8972cc5ae182cb70181d60bd7aa6f
Max Poletaev <max.poletaev@gmail.com>
2026-01-25 00:39:29 +0300

    Add check step

--- build.zig
+++ build.zig
@@ -4,61 +4,63 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
+    const zglfw = b.dependency("zglfw", .{ .target = target });
+    const zgui = b.dependency("zgui", .{ .target = target, .backend = .glfw_opengl3 });
+    const zaudio = b.dependency("zaudio", .{ .target = target });
+    const zopengl = b.dependency("zopengl", .{});
+
+    const nupsx_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    nupsx_mod.addImport("zglfw", zglfw.module("root"));
+    nupsx_mod.addImport("zopengl", zopengl.module("root"));
+    nupsx_mod.addImport("zgui", zgui.module("root"));
+    nupsx_mod.addImport("zaudio", zaudio.module("root"));
 
-    const exe = b.addExecutable(.{
+    const nupsx_exe = b.addExecutable(.{
         .name = "nupsx",
-        .root_module = exe_mod,
+        .root_module = nupsx_mod,
     });
-    b.installArtifact(exe);
+    nupsx_exe.linkLibrary(zgui.artifact("imgui"));
+    nupsx_exe.linkLibrary(zglfw.artifact("glfw"));
+    nupsx_exe.linkLibrary(zaudio.artifact("miniaudio"));
 
-    // zig-gamedev
+    b.installArtifact(nupsx_exe);
+
+    // run
     {
-        const zglfw = b.dependency("zglfw", .{
-            .target = target,
-        });
-        exe.root_module.addImport("zglfw", zglfw.module("root"));
-        exe.linkLibrary(zglfw.artifact("glfw"));
+        const run_cmd = b.addRunArtifact(nupsx_exe);
+        run_cmd.step.dependOn(b.getInstallStep());
 
-        const zopengl = b.dependency("zopengl", .{});
-        exe.root_module.addImport("zopengl", zopengl.module("root"));
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
 
-        const zmath = b.dependency("zmath", .{});
-        exe.root_module.addImport("zmath", zmath.module("root"));
-
-        const zgui = b.dependency("zgui", .{
-            .target = target,
-            .backend = .glfw_opengl3,
-        });
-        exe.root_module.addImport("zgui", zgui.module("root"));
-        exe.linkLibrary(zgui.artifact("imgui"));
-
-        const zaudio = b.dependency("zaudio", .{
-            .target = target,
-        });
-        exe.root_module.addImport("zaudio", zaudio.module("root"));
-        exe.linkLibrary(zaudio.artifact("miniaudio"));
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
+    // test
+    {
+        const exe_unit_tests = b.addTest(.{
+            .root_module = nupsx_mod,
+        });
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_exe_unit_tests.step);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    // check (required for zls)
+    // see: https://zigtools.org/zls/guides/build-on-save/
+    {
+        const check_exe = b.addExecutable(.{
+            .name = "nupsx_check",
+            .root_module = nupsx_mod,
+        });
+        const check = b.step("check", "Check if foo compiles");
+        check.dependOn(&check_exe.step);
+    }
 }
