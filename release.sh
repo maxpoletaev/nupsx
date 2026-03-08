efcbf5163aa056172f545bd700c54976e4005b1b
Max Poletaev <max.poletaev@gmail.com>
2026-03-08 10:56:12 +0300

    Update release script

--- release.sh
+++ release.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-VERSION="$1"
-if [ -z "$VERSION" ]; then
-  echo "Usage: $0 <version>"
-  exit 1
-fi
-
 TARGETS=(
     "aarch64-macos"
     "aarch64-linux-gnu"
@@ -16,6 +10,7 @@ TARGETS=(
 )
 
 mkdir -p dist
+DIST_DIR=`cd dist && pwd`
 
 for target in "${TARGETS[@]}"; do
     if [ -d "zig-out/$target" ]; then
@@ -24,5 +19,5 @@ for target in "${TARGETS[@]}"; do
     fi
     echo "Building for $target..."
     zig build -Dtarget="$target" --prefix "zig-out/$target" --release=safe --prefix-exe-dir .
-    zip -r "dist/nupsx-${VERSION}-${target}.zip" "zig-out/${target}/"
+    (cd "zig-out/$target/bin" && zip -r "$DIST_DIR/nupsx-$target.zip" .)
 done
