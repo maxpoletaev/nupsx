d7fdd4e5937da3a2a8681c662da20265443f12c5
Max Poletaev <max.poletaev@gmail.com>
2026-05-12 20:49:05 +0300

    Put tty under a flag

--- build.zig
+++ build.zig
@@ -11,6 +11,7 @@ pub fn build(b: *std.Build) void {
 
     const options = b.addOptions();
     options.addOption(bool, "uncapped", b.option(bool, "uncapped", "Run uncapped (no audio/video sync)") orelse false);
+    options.addOption(bool, "tty", b.option(bool, "tty", "Enable TTY output") orelse false);
 
     const nupsx_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
