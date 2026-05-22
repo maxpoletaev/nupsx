c47e2e8f45907a37c6e6236f0c65a6d8fdd1af07
Max Poletaev <max.poletaev@gmail.com>
2026-05-23 00:26:39 +0300

    Update zig-gamedev libs

--- src/debug_ui/DebugUI.zig
+++ src/debug_ui/DebugUI.zig
@@ -45,7 +45,7 @@ last_frame_time: f64 = 0,
 next_frame_time: f64 = 0,
 is_running: bool = true,
 
-pub fn init(allocator: std.mem.Allocator, io: std.Io, cpu: *CPU, bus: *Bus) !*@This() {
+pub fn init(allocator: std.mem.Allocator, cpu: *CPU, bus: *Bus) !*@This() {
     try glfw.init();
     glfw.windowHint(.context_version_major, gl_version[0]);
     glfw.windowHint(.context_version_minor, gl_version[1]);
@@ -65,7 +65,7 @@ pub fn init(allocator: std.mem.Allocator, io: std.Io, cpu: *CPU, bus: *Bus) !*@T
 
     try zopengl.loadCoreProfile(glfw.getProcAddress, gl_version[0], gl_version[1]);
 
-    zgui.init(io, allocator);
+    zgui.init(allocator);
     zgui.backend.init(window);
 
     const style = zgui.getStyle();
--- src/main.zig
+++ src/main.zig
@@ -274,7 +274,7 @@ pub fn main(init: std.process.Init) !void {
     }
 
     if (args.debug) {
-        const debug_ui = try DebugUI.init(allocator, io, cpu, bus);
+        const debug_ui = try DebugUI.init(allocator, cpu, bus);
         defer debug_ui.deinit();
 
         while (debug_ui.is_running) {
